bd2b73cfb8d9f7fefbf6d0115c7b5289c55a1f5c
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-18 20:58:05 +0900

    devise_setting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @articles = Article.all
