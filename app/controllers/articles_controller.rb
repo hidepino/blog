c46b9b1c179a5ffa8d9da944421f9a17ab00eea2
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-18 20:07:54 +0900

    article_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,41 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
+
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:text)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
