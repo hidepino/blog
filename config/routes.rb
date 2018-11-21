d956d6d9fd69be5da0b5a31bac295c80240f5927
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-21 15:10:16 +0900

    create_mypage

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :articles, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+  resources :users, only: [:show]
 end
