eb781e36f35e8b3a630920a49429cf5a19a602b4
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-21 18:34:19 +0900

    add_comment_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
-  resources :articles, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+  resources :articles, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
+    resources :comments, only: [:new, :create]
+  end
   resources :users, only: [:show]
 end
