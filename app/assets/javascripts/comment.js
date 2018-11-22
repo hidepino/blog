62c9df3ced5d89e18a265f27b327d42e598d09aa
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-22 16:57:05 +0900

    Son

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,30 @@
+$(function(){
+  $(document).on('turbolinks:load', function(){
+    function buildHTML(comment){
+      var html = `<p>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ${comment.text}
+                  </p>`
+      return html;
+    }
+
+    $("#new_comment").on("submit", function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json'
+      })
+      .done(function(comment){
+        // console.log(comment);
+        var html = buildHTML(comment);
+        $('#comment-list').append(html);
+        $('.textbox')[0].reset();
+      })
+      .fail(alert('error'));
+    });
+  })
+});
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    redirect_to "/articles/#{@comment.article.id}"
+    respond_to do |format|
+      format.html { redirect_to "/articles/#{@comment.article.id}" }
+      format.json
+    end
   end
 
   private
