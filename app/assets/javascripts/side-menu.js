38df1a01128ae1da50560a05597905a3a6950607
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-24 12:16:51 +0900

    edit

--- app/assets/javascripts/side-menu.js
+++ app/assets/javascripts/side-menu.js
@@ -2,7 +2,7 @@ $(function () {
   $(document).on('turbolinks:load', function(){
     $('#js__sideMenuBtn').on('click', function () {
       $('.container').toggleClass('side-open');
-      $('.side-menu').toggleClass('side-menu--close');
+      $('.side-menu').toggleClass('side-menu--open');
     });
   })
 });
